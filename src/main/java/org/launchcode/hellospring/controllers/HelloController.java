f5463abdfc03c2c8e5ea947d049309c7a9627482
Katie Walsh <kwalsh7642@gmail.com>
2022-10-09 15:09:14 -0500

    added mapping for a path parameter

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,6 +2,7 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -28,4 +29,14 @@ public class HelloController {
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
+
+    //handler that handles requests of the form /hello/LaunchCode (path parameter)
+
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParameter(@PathVariable String name){
+        return "Hello, " + name + "!";
+
+    }
+
 }
