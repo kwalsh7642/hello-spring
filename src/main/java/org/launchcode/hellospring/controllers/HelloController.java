29909e64b1f59521a37a3cdeba46bac02cae4311
Katie Walsh <kwalsh7642@gmail.com>
2022-10-09 14:58:13 -0500

    added query parameters

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,13 +2,14 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     // Handles requests at path /hello
-    @GetMapping("hello")
+    @GetMapping("hello1")
     @ResponseBody
     public String hello() {
         return "Hello, Spring!";
@@ -18,5 +19,13 @@ public class HelloController {
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
+    }
+
+    // Dynamic handler handles requests of the for /hello?name=LaunchCode
+
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 }
